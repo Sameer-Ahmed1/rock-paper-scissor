33d0cc3fe38cc8deaef77289b78a56e62a372d35
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2022-08-09 20:51:35 +0530

    Add click event listener

--- external.js
+++ external.js
@@ -1,22 +1,71 @@
-function getComputerChoice (choices)
-{
-    return choices[Math.floor(Math.random()*choices.length)]
-}
-//console.log(getComputerChoice(['Rock', 'Paper', 'Scissors']));
-function playRound(playerSelection, computerSelection=getComputerChoice(['ROCK', 'PAPER', 'SCISSORS']))
-{
-    if(playerSelection!=null)
-    playerSelection=playerSelection.toUpperCase();
-    if(playerSelection===computerSelection)
-        return `It's a tie!`;
-    else if((playerSelection==='ROCK'&&computerSelection==='SCISSORS')||(playerSelection==='PAPER'&&computerSelection==='ROCK')||(playerSelection==='SCISSOR'&&computerSelection=='PAPER'))
-        return `You won ${playerSelection} beats ${computerSelection}`;
-    else 
-        return `You lose ${computerSelection} beats ${playerSelection}`;
+//create two global variables to store each players score
+let playerScore = 0,
+  computerScore = 0;
+
+//generate random choice
+function getComputerChoice(choices) {
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-for(let i=0; i<5; i++)
-{
-    let playerSelection=prompt("Your choice: ");
-    console.log(playRound(playerSelection));
+function playRound(event) {
+  //declare a variable playerSelection and store the event nodes class
+  let playerSelection = event.target.classList.value;
+
+  //take the computer choice in another variable
+  let computerSelection = getComputerChoice(["ROCK", "PAPER", "SCISSOR"]);
+
+  //create two variables that'll reference to divs result and score
+  const resultDiv = document.querySelector(".result");
+  const scoreDiv = document.querySelector(".score");
+  let result;
+
+  //after any condition is met, increment each player's score and update the result
+  if (playerSelection != null) playerSelection = playerSelection.toUpperCase();
+  if (playerSelection === computerSelection) {
+    result = `It's a tie!`;
+  } else if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSOR") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSOR" && computerSelection == "PAPER")
+  ) {
+    result = `You won ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
+  } else {
+    result = `You lose ${computerSelection} beats ${playerSelection}`;
+    computerScore++;
+  }
+
+  //update div.result
+  resultDiv.textContent = result;
+  //update score
+  scoreDiv.textContent = `You : ${playerScore} Computer : ${computerScore}`;
 }
+
+function playGame() {
+  let counter = 0;
+  //call the addeventlistener  'click'  and update the score div consequently
+  const options = document.querySelector(".options");
+  options.addEventListener("click", (event) => {
+    const isButton = event.target.nodeName === "BUTTON";
+    if (!isButton || counter >= 5) {
+      return;
+    }
+    counter++;
+    const round = document.querySelector(".round");
+    playRound(event);
+    if (counter < 5) {
+      round.textContent = `Round ${counter + 1}`;
+    } else {
+      displayWinner();
+    }
+  });
+}
+
+//after game ends
+function displayWinner() {
+  const round = document.querySelector(".round");
+  if (computerScore > playerScore) round.textContent = "Computer won :/";
+  else if (playerScore > computerScore) round.textContent = "You won!";
+  else round.textContent = "Nobody won the game ";
+}
+playGame();
